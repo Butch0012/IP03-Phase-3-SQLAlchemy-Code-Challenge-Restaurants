14f2c4d83997d786d5b6e67a73c1e5bd8213e8ce
butch0012 <elnathanmburu@gmail.com>
2024-01-08 11:04:04 +0300

    define review class

--- main.py
+++ main.py
@@ -9,3 +9,13 @@ engine = create_engine("sqlite:///db.db")
 
 # Declare a base class for declarative models
 Base = declarative_base()
+# Define the Review class
+class Review(Base):
+    __tablename__ = "reviews"
+
+    id = Column(Integer, primary_key=True)
+    star_rating = Column(Integer)
+    restaurant_id = Column(Integer, ForeignKey("restaurants.id"))
+    customer_id = Column(Integer, ForeignKey("customers.id"))
+    restaurant = relationship("Restaurant", back_populates="reviews")
+    customer = relationship("Customer", back_populates="reviews")
