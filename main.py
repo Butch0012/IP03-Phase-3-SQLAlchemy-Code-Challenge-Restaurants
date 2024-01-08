3d3621af55d31a91fb41887fe2251d6957dd0e00
butch0012 <elnathanmburu@gmail.com>
2024-01-08 10:43:27 +0300

    Create the SQLite database engine

---
+++ main.py
@@ -0,0 +1,8 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.sql import func
+import random
+
+# Create the SQLite database engine
+engine = create_engine("sqlite:///db.db")
